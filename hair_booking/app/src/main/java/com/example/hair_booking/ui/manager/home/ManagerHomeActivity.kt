002438ab7bea6ea4a8e74fa6093dbcef3abd0908
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-03-28 00:22:39 +0700

    custom navigation drawer

--- hair_booking/app/src/main/java/com/example/hair_booking/ui/manager/home/ManagerHomeActivity.kt
+++ hair_booking/app/src/main/java/com/example/hair_booking/ui/manager/home/ManagerHomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.hair_booking.ui.manager.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hair_booking.R
 
 class ManagerHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
